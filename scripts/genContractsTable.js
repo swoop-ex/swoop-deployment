10e97d9f65b161d50333c6da082861c4d00f9327
peekpi <894646171@qq.com>
2020-11-04 20:25:00 +0800

    update README

--- scripts/genContractsTable.js
+++ scripts/genContractsTable.js
@@ -1,4 +1,5 @@
 const crypto = require('@harmony-js/crypto');
+const defualtTokens = require('@swoop-exchange/default-token-list');
 const Fs=require('fs');
 
 const File = e=>Fs.readFileSync(e).toString()
@@ -10,15 +11,19 @@ function readContracts(filename) {
     //console.log(context);
     //return;
     eval(context);
+    const chainId = NETWORK == 'mainnet' ? 1 : 2;
     const obj = {};
     obj.UNISWAPV2FACTORY = UNISWAPV2FACTORY;
+
+    defualtTokens.tokens.filter(e=>e.chainId==chainId).forEach(e=>obj[e.symbol]=e.address);
     obj.Multicall = MULTICALL;
+    /*
     obj.WONE = WONE;
     obj.OneBUSD = ONEBUSD;
     obj.OneBTC = ONEBTC;
     obj.OneETH = ONEETH;
     obj.OneChainlink = ONECHAINLINK;
-    obj.OneSeed = ONESEED;
+    obj.OneSeed = ONESEED;*/
     obj.UNISWAPV2ROUTER02 = UNISWAPV2ROUTER02;
     contracts[NETWORK] = obj;
 }
@@ -26,16 +31,20 @@ function readContracts(filename) {
 readContracts('mainnet_contract_deployed.txt');
 readContracts('testnet_contract_deployed.txt');
 
+console.log(contracts.mainnet)
+
 table = `
 | Contract     | Testnet                                                                                 | Mainnet     |
 | -----------  | --------------------------------------------------------------------------------------- | ----------- |
+| FACTORY      | ${contracts.testnet.UNISWAPV2FACTORY} (${crypto.toBech32(contracts.testnet.UNISWAPV2FACTORY)}) | ${contracts.mainnet.UNISWAPV2FACTORY} (${crypto.toBech32(contracts.mainnet.UNISWAPV2FACTORY)}) |
+| ROUTER02     | ${contracts.testnet.UNISWAPV2ROUTER02} (${crypto.toBech32(contracts.testnet.UNISWAPV2ROUTER02)}) | ${contracts.mainnet.UNISWAPV2ROUTER02} (${crypto.toBech32(contracts.mainnet.UNISWAPV2ROUTER02)}) |
 | Multicall    | ${contracts.testnet.Multicall} (${crypto.toBech32(contracts.testnet.Multicall)}) | ${contracts.mainnet.Multicall} (${crypto.toBech32(contracts.mainnet.Multicall)}) |
 | WONE         | ${contracts.testnet.WONE} (${crypto.toBech32(contracts.testnet.WONE)}) | ${contracts.mainnet.WONE} (${crypto.toBech32(contracts.mainnet.WONE)}) |
-| OneBUSD      | ${contracts.testnet.OneBUSD} (${crypto.toBech32(contracts.testnet.OneBUSD)}) | ${contracts.mainnet.OneBUSD} (${crypto.toBech32(contracts.mainnet.OneBUSD)}) |
-| OneBTC       | ${contracts.testnet.OneBTC} (${crypto.toBech32(contracts.testnet.OneBTC)}) | ${contracts.mainnet.OneBTC} (${crypto.toBech32(contracts.mainnet.OneBTC)}) |
-| OneETH       | ${contracts.testnet.OneETH} (${crypto.toBech32(contracts.testnet.OneETH)}) | ${contracts.mainnet.OneETH} (${crypto.toBech32(contracts.mainnet.OneETH)}) |
-| OneChainlink | ${contracts.testnet.OneChainlink} (${crypto.toBech32(contracts.testnet.OneChainlink)}) | ${contracts.mainnet.OneChainlink} (${crypto.toBech32(contracts.mainnet.OneChainlink)}) |
-| OneSeed      | ${contracts.testnet.OneSeed} (${crypto.toBech32(contracts.testnet.OneSeed)}) | ${contracts.mainnet.OneSeed} (${crypto.toBech32(contracts.mainnet.OneSeed)}) |
+| OneBUSD      | ${contracts.testnet['1BUSD']} (${crypto.toBech32(contracts.testnet['1BUSD'])}) | ${contracts.mainnet.BUSD} (${crypto.toBech32(contracts.mainnet.BUSD)}) |
+| OneBTC       | ${contracts.testnet['1BTC']} (${crypto.toBech32(contracts.testnet['1BTC'])}) | ${contracts.mainnet['1WBTC']} (${crypto.toBech32(contracts.mainnet['1WBTC'])}) |
+| OneETH       | ${contracts.testnet['1ETH']} (${crypto.toBech32(contracts.testnet['1ETH'])}) | ${contracts.mainnet['1WETH']} (${crypto.toBech32(contracts.mainnet['1WETH'])}) |
+| OneLINK      | ${contracts.testnet['1LINK']} (${crypto.toBech32(contracts.testnet['1LINK'])}) | ${contracts.mainnet.LINK} (${crypto.toBech32(contracts.mainnet.LINK)}) |
+| OneSEED      | ${contracts.testnet['1SEED']} (${crypto.toBech32(contracts.testnet['1SEED'])}) | -  |
 `
 
 console.log(table);
